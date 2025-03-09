a81ee4f3f68f5ebf333c1665cf38247ab15d325c
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2025-03-09 16:18:59 +0500

    disable eslin

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,7 +2,12 @@ import { NextApiResponse } from "next";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 export async function GET(req: Request, res: NextApiResponse) {
-    const cookieStore = await cookies();
-    cookieStore.delete("task-user");
-    return NextResponse.json({ res: "success" }, { status: 200 });
+    try {
+        const cookieStore = await cookies();
+        cookieStore.delete("task-user");
+        return NextResponse.json({ res: "success" }, { status: 200 });
+    } catch (e) {
+        console.error("Error fetching user data:", e);
+        return NextResponse.json({ res: "error" }, { status: 500 });
+    }
 }
